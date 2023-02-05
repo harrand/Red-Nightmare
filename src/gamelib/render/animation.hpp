8e9b33f3fc1de7982e44cff97d2aa3cc86a5c051
Harrand <harrysjh98@gmail.com>
2023-02-05 19:05:57 +0000

    + Added skeleton animation.hpp/cpp but NYI

---
+++ src/gamelib/render/animation.hpp
@@ -0,0 +1,17 @@
+#ifndef RNLIB_RENDER_ANIMATION_HPP
+#define RNLIB_RENDER_ANIMATION_HPP
+
+namespace rnlib
+{
+	struct animation_data
+	{
+		
+	};
+
+	class animation
+	{
+
+	};
+}
+
+#endif // RNLIB_RENDER_ANIMATION_HPP
