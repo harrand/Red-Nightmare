43ae3e10142bc805cf9115b9a85ad87923086243
harrand <harrysjh98@gmail.com>
2023-05-07 00:47:34 +0100

    * Implemented death animation for human/hair/lord

--- src/gamelib/render/image.cpp
+++ src/gamelib/render/image.cpp
@@ -111,6 +111,16 @@ namespace rnlib
 #include ImportedTextHeader(human_hair_lord_down3, png)
 #include ImportedTextHeader(human_hair_lord_cast0, png)
 #include ImportedTextHeader(human_hair_lord_cast1, png)
+#include ImportedTextHeader(human_hair_lord_death00, png)
+#include ImportedTextHeader(human_hair_lord_death01, png)
+#include ImportedTextHeader(human_hair_lord_death02, png)
+#include ImportedTextHeader(human_hair_lord_death03, png)
+#include ImportedTextHeader(human_hair_lord_death04, png)
+#include ImportedTextHeader(human_hair_lord_death05, png)
+#include ImportedTextHeader(human_hair_lord_death06, png)
+#include ImportedTextHeader(human_hair_lord_death07, png)
+#include ImportedTextHeader(human_hair_lord_death08, png)
+#include ImportedTextHeader(human_hair_lord_death09, png)
 #include ImportedTextHeader(human_hair_prince_idle0, png)
 #include ImportedTextHeader(human_hair_prince_idle1, png)
 #include ImportedTextHeader(human_hair_prince_side0, png)
@@ -377,6 +387,16 @@ namespace rnlib
 		ImportedTextData(human_hair_lord_down3, png),
 		ImportedTextData(human_hair_lord_cast0, png),
 		ImportedTextData(human_hair_lord_cast1, png),
+		ImportedTextData(human_hair_lord_death00, png),
+		ImportedTextData(human_hair_lord_death01, png),
+		ImportedTextData(human_hair_lord_death02, png),
+		ImportedTextData(human_hair_lord_death03, png),
+		ImportedTextData(human_hair_lord_death04, png),
+		ImportedTextData(human_hair_lord_death05, png),
+		ImportedTextData(human_hair_lord_death06, png),
+		ImportedTextData(human_hair_lord_death07, png),
+		ImportedTextData(human_hair_lord_death08, png),
+		ImportedTextData(human_hair_lord_death09, png),
 		ImportedTextData(human_hair_prince_idle0, png),
 		ImportedTextData(human_hair_prince_idle1, png),
 		ImportedTextData(human_hair_prince_side0, png),
