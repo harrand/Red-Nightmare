d28ad23f9d78055e3ee26b8ac20715e6e3ea5527
harrand <harrysjh98@gmail.com>
2023-05-29 17:54:24 +0100

    * component_id::level_background conditionally mounts quads only when necessary

--- src/gamelib/gameplay/actor/components/level_background.hpp
+++ src/gamelib/gameplay/actor/components/level_background.hpp
@@ -1,8 +1,8 @@
 template<>
 struct actor_component_params<actor_component_id::level_background>
 {
-	int background_texid = 0;
-	int foreground_texid = 0;
+	int background_texid = image_id::undefined;
+	int foreground_texid = image_id::invisible;
 };
 
 template<>
@@ -12,20 +12,28 @@ inline mount_result actor_component_mount<actor_component_id::level_background>
 	std::span<quad_renderer::quad_data> quads
 )
 {
-	auto& bg = quads[0];
-	bg.texid[0] = component.data().background_texid;
-	bg.tints[0] = tz::vec4::filled(1.0f);
-	bg.scale = tz::vec2::filled(1.0f);
-	auto& fg = quads[1];
-	fg.texid[0] = component.data().foreground_texid;
-	fg.tints[0] = tz::vec4::filled(1.0f);
-	fg.scale = tz::vec2::filled(1.0f);
-	return {.count = 2};
+	std::size_t count = 0;
+	if(component.data().background_texid != image_id::invisible)
+	{
+		auto& bg = quads[count++];
+		bg.texid[0] = component.data().background_texid;
+		bg.tints[0] = tz::vec4::filled(1.0f);
+		bg.scale = tz::vec2::filled(1.0f);
+		bg.layer = 1;
+	}
+	if(component.data().foreground_texid != image_id::invisible)
+	{
+		auto& fg = quads[count++];
+		fg.texid[0] = component.data().foreground_texid;
+		fg.tints[0] = tz::vec4::filled(1.0f);
+		fg.scale = tz::vec2::filled(1.0f);
+	}
+	return {.count = count};
 }
 
 template<>
 inline void actor_component_dbgui(actor_component<actor_component_id::level_background>& component)
 {
-	ImGui::SliderInt("Background Image", &component.data().background_texid, 0, image_id::_count);
-	ImGui::SliderInt("Foreground Image", &component.data().foreground_texid, 0, image_id::_count);
+	ImGui::SliderInt("Background Image", &component.data().background_texid, 0, image_id::_count - 1);
+	ImGui::SliderInt("Foreground Image", &component.data().foreground_texid, 0, image_id::_count - 1);
 }
