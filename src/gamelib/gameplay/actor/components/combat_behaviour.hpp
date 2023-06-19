50e305b105db324540062df7539d80d8c2609eac
harrand <harrysjh98@gmail.com>
2023-06-19 13:17:09 +0100

    * Improved combat_behaviour to be a little more customiseable

--- src/gamelib/gameplay/actor/components/combat_behaviour.hpp
+++ src/gamelib/gameplay/actor/components/combat_behaviour.hpp
@@ -6,12 +6,51 @@ enum class combat_behaviour_flag
 };
 using combat_behaviour_flags = tz::enum_field<combat_behaviour_flag>;
 
+struct combat_spell
+{
+	spell_id spellid;
+	float cooldown_seconds = 0.0f;
+	float impl_current_cd = 0.0f;
+};
+
 constexpr float fireball_cd_seconds = 2.0f;
 template<>
 struct actor_component_params<actor_component_id::combat_behaviour>
 {
 	combat_behaviour_flags flags = {};
-	float impl_fireball_cd = fireball_cd_seconds;
+	std::vector<combat_spell> aggressive_ranged_spells = {};
+	std::vector<combat_spell> aggressive_melee_spells = {};
+	float defensive_hp_ratio_cutoff = 0.5f;
+	std::vector<combat_spell> defensive_ranged_spells = {};
+	std::vector<combat_spell> defensive_melee_spells = {};
+
+	combat_spell* get_aggressive_spell()
+	{
+		auto iter = std::find_if(this->aggressive_ranged_spells.begin(), this->aggressive_ranged_spells.end(),
+		[](const combat_spell& sp)
+		{
+			return sp.impl_current_cd <= 0.0f;
+		});
+		if(iter == this->aggressive_ranged_spells.end())
+		{
+			return nullptr;
+		}
+		return &*iter;
+	}
+
+	combat_spell* get_defensive_spell()
+	{
+		auto iter = std::find_if(this->defensive_ranged_spells.begin(), this->defensive_ranged_spells.end(),
+		[](const combat_spell& sp)
+		{
+			return sp.impl_current_cd <= 0.0f;
+		});
+		if(iter == this->defensive_ranged_spells.end())
+		{
+			return nullptr;
+		}
+		return &*iter;
+	}
 };
 
 template<>
@@ -23,6 +62,22 @@ inline void actor_component_update<actor_component_id::combat_behaviour>
 )
 {
 	TZ_PROFZONE("combat behaviour component - update", 0xffaa0077);
+	for(combat_spell& cs : component.data().aggressive_ranged_spells)
+	{
+		cs.impl_current_cd -= (dt / 1000.0f);
+	}
+	for(combat_spell& cs : component.data().aggressive_melee_spells)
+	{
+		cs.impl_current_cd -= (dt / 1000.0f);
+	}
+	for(combat_spell& cs : component.data().defensive_ranged_spells)
+	{
+		cs.impl_current_cd -= (dt / 1000.0f);
+	}
+	for(combat_spell& cs : component.data().defensive_melee_spells)
+	{
+		cs.impl_current_cd -= (dt / 1000.0f);
+	}
 	auto stop_movement = [&actor_me]()
 	{
 		if(actor_me.actions.has_component<action_id::move_to>())
@@ -47,15 +102,20 @@ inline void actor_component_update<actor_component_id::combat_behaviour>
 	{
 		return;
 	}
+	combat_spell* def_spell = component.data().get_defensive_spell();
 	if(component.data().flags.contains(combat_behaviour_flag::heal_when_low)
 	&& actor_me.entity.has_component<actor_component_id::damageable>()
-	&& actor_me.entity.get_component<actor_component_id::damageable>()->data().hp_pct() < 0.5f)
+	&& actor_me.entity.get_component<actor_component_id::damageable>()->data().hp_pct() < 0.5f
+	&& component.data().defensive_ranged_spells.size()
+	&& def_spell != nullptr)
 	{
 		stop_movement();
+		// find a defensive on cooldown.
 		actor_me.actions.add_component<action_id::cast>
 		({
-			.spell = spell_id::heal
+			.spell = def_spell->spellid
 		});
+		def_spell->impl_current_cd = def_spell->cooldown_seconds;
 	}
 	else
 	{
@@ -64,8 +124,8 @@ inline void actor_component_update<actor_component_id::combat_behaviour>
 		({
 			.predicate = [](const actor& me, const actor& other)
 			{
-				return (other.entity.has_component<actor_component_id::damageable>() && !other.entity.get_component<actor_component_id::damageable>()->data().dead())
-						&& get_allegience(me.faction, other.faction) == allegience_t::enemy;
+				return !other.dead()
+				&& get_allegience(me.faction, other.faction) == allegience_t::enemy;
 			}
 		});
 
@@ -74,16 +134,16 @@ inline void actor_component_update<actor_component_id::combat_behaviour>
 		{
 			actor_me.actions.add_component<action_id::move_to_target>();
 		}
-		component.data().impl_fireball_cd -= (dt / 1000.0f);
+		combat_spell* aggro_spell = component.data().get_aggressive_spell();
 		if(component.data().flags.contains(combat_behaviour_flag::sometimes_cast_fireball)
-		&& component.data().impl_fireball_cd <= 0.0f)
+		&& aggro_spell != nullptr)
 		{
-			component.data().impl_fireball_cd = fireball_cd_seconds;
 			stop_movement();
 			actor_me.actions.add_component<action_id::cast>
 			({
-				.spell = spell_id::fireball
+				.spell = aggro_spell->spellid
 			});
+			aggro_spell->impl_current_cd = aggro_spell->cooldown_seconds;
 		}
 	}
 }
