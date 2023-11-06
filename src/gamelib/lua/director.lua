d4896b41f59def0548f8aebe222effd059c0ee56
harrand <harrysjh98@gmail.com>
2023-11-06 22:25:51 +0000

    [director] variable credit acquisition rate

--- src/gamelib/lua/director.lua
+++ src/gamelib/lua/director.lua
@@ -4,6 +4,8 @@ rn.director.credit = 0
 rn.director.second_timeout = 0
 rn.director.spawn_cooldown = 0
 rn.director.paused = false
+rn.director.rate_counter = 0
+rn.director.rate = 1
 
 rn.fixed_update = function()
 	tracy.ZoneBegin()
@@ -22,7 +24,14 @@ end
 rn.director.on_second_pass = function()
 	-- director gains 1 credit per second
 	if rn.director.paused then return end
-	rn.director.credit = rn.director.credit + 2
+	rn.director.credit = rn.director.credit + rn.director.rate
+	rn.director.rate_counter = rn.director.rate_counter + 1
+
+	-- every 30 seconds, rate increases
+	if rn.director.rate_counter >= 30 then
+		rn.director.rate = rn.director.rate + 1
+		rn.director.rate_counter = 0
+	end
 	rn.director.spawn_cooldown = rn.director.spawn_cooldown - 1
 
 	if rn.director.spawn_cooldown <= 0 then
@@ -44,12 +53,27 @@ rn.director.on_second_pass = function()
 			rn.director.spawn_cooldown = 15
 		end
 
-		-- cost 25: banshee
-		if rn.director.credit > 25 then
-			local credit_cost = 25
+		-- cost 50: banshee
+		if rn.director.credit > 50 then
+			local credit_cost = 50
 			rn.director.credit = rn.director.credit - credit_cost
 			rn.scene():add(11)
 			rn.director.spawn_cooldown = 15
+		end
+
+		-- cost 100: horde of rushers
+		if rn.director.credit > 100 then
+			local credit_cost = 100
+			rn.director.credit = rn.director.credit - credit_cost
+			for i=0,3,1 do
+				local zomb = rn.scene():get(rn.scene():add(2))
+				rn.equip(zomb, "White Legion Helmet")
+				local speedy = rn.new_buff()
+				speedy:set_increased_movement_speed(150)
+				zomb:apply_buff(speedy)
+				zomb:get_element():set_uniform_scale(zomb:get_element():get_uniform_scale() * 0.7)
+			end
+			rn.director.spawn_cooldown = 7
 		end
 
 		-- cost 200: OMEGA zombie
