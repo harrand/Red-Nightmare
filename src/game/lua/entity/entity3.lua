275aa1c31f90bc706a7e1534183f586a36dac280
harrand <harrysjh98@gmail.com>
2023-11-09 00:46:56 +0000

    [lua.entity3] dropped loot now emits a small amount of light

--- src/game/lua/entity/entity3.lua
+++ src/game/lua/entity/entity3.lua
@@ -18,9 +18,17 @@ rn.entity_handler[id] =
 		ent:get_element():set_uniform_scale(sc * 0.3)
 		ent:get_element():object_set_visibility(3, false)
 		ent:get_element():face_forward()
+
+		local data = rn.entity_get_data(ent)
+		data.impl.light = rn.scene():add_light()
+		data.impl.light:set_power(0.4)
+	end,
+	deinit = function(ent)
+		rn.scene():remove_light(rn.entity_get_data(ent).impl.light)
 	end,
 	update = function(ent)
 		local data = rn.entity.data[ent:uid()]
+		data.impl.light:set_position(ent:get_element():get_subobject_position(7))
 		rn.for_each_collision(ent, function(ent2)
 			if not data.collided_this_update and ent2:get_model() == rn.model.humanoid and ent2:get_type() ~= ent:get_type() and not ent2:is_dead() then
 				-- equip whatever we're wearing onto ent2
