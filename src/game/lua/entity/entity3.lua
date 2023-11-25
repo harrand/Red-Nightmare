fef7285d41813394153c1903dd39a79cbff0585c
harrand <harrysjh98@gmail.com>
2023-11-25 06:15:27 +0000

    [lua.entity3] impl.counter is now in the datastore

--- src/game/lua/entity/entity3.lua
+++ src/game/lua/entity/entity3.lua
@@ -23,17 +23,18 @@ rn.entity_handler[id] =
 		local light_id = rn.scene():add_light()
 		local light = rn.scene():get_light(light_id)
 		light:set_power(0.4)
-		data.impl.counter = 0
-		rn.entity_data_write(ent, "impl.targetable", false, "impl.projectile_skip", true, "impl.light", light_id)
+		rn.entity_data_write(ent, "impl.targetable", false, "impl.projectile_skip", true, "impl.light", light_id, "impl.counter", 0)
 	end,
 	deinit = function(ent)
 		rn.scene():remove_light(rn.entity_data_read(ent, "impl.light"))
 	end,
 	update = function(ent)
 		local data = rn.entity.data[ent:uid()]
-		data.impl.counter = data.impl.counter + rn.delta_time
+		local counter = rn.entity_data_read(ent, "impl.counter")
+		counter = counter + rn.delta_time
+		rn.entity_data_write(ent, "impl.counter", counter)
 		ent:get_element():face_forward()
-		ent:get_element():vrotate(data.impl.counter * 2.2)
+		ent:get_element():vrotate(counter * 2.2)
 		local light = rn.scene():get_light(rn.entity_data_read(ent, "impl.light"))
 		light:set_position(ent:get_element():get_subobject_position(7))
 		rn.for_each_collision(ent, function(ent2)
