2b6e31067f409cc40fc9637ec66186cb4ec2eb94
harrand <harrysjh98@gmail.com>
2024-01-27 20:56:04 +0000

    [entity] add ability to get/set internal variables directly in c++

--- src/gamelib/entity.hpp
+++ src/gamelib/entity.hpp
@@ -1,6 +1,7 @@
 #ifndef REDNIGHTMARE_GAMELIB_ENTITY_HPP
 #define REDNIGHTMARE_GAMELIB_ENTITY_HPP
 #include "tz/lua/state.hpp"
+#include "tz/core/algorithms/static.hpp"
 #include <limits>
 #include <cstdint>
 #include <string>
@@ -24,6 +25,24 @@ namespace game
 		bool is_null() const
 		{
 			return this->uuid == null_uuid;
+		}
+
+		// set an internal variable. if the variable doesnt exist, also returns nil.
+		template<typename T>
+		tz::lua::lua_generic get_internal(std::string_view varname) const
+		{
+			auto iter = this->internal_variables.find(varname);
+			if(iter == this->internal_variables.end())
+			{
+				return tz::lua::nil{};
+			}
+			return iter->second;
+		}
+
+		template<typename T>
+		void set_internal(std::string_view varname, T&& val) requires(tz::static_find<std::decay_t<T>, bool, double, std::int64_t, std::string>() || std::is_integral_v<T> || std::is_same_v<T, tz::lua::nil>)
+		{
+			this->internal_variables[varname] = val; 
 		}
 	};
 }
