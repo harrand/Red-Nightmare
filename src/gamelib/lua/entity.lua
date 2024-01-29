c29e5ba90b0b76c535592f362de82b12970ea7e4
harrand <harrysjh98@gmail.com>
2024-01-29 01:35:58 +0000

    [lua.prefab] prefab pre-instantiate and instantiate calling. if you attempt to instantiate an entity from an unknown prefab, is error

--- src/gamelib/lua/entity.lua
+++ src/gamelib/lua/entity.lua
@@ -1,10 +1,21 @@
 rn = rn or {}
 rn.entity = rn.entity or {}
+rn.entity.prefabs = rn.entity.prefabs or {}
 
 rn.entity.pre_instantiate = function(uuid, prefab_name)
 	local obj <close> = tz.profzone_obj:new()
 	obj:set_text(tostring(uuid))
 	obj:set_name("Pre-Instantiate - \"" .. prefab_name .. "\"")
+
+	local prefab = rn.entity.prefabs[prefab_name]
+	if prefab ~= nil then
+		if prefab.pre_instantiate ~= nil then
+			prefab.pre_instantiate(uuid)
+		end
+	else
+		tz.report("Missing prefab \"" .. prefab_name .. "\"")
+		tz.assert(false);
+	end
 
 	tz.report("pre_instantiate " .. tostring(uuid) .. " from \"" .. prefab_name .. "\"")
 
@@ -15,6 +26,16 @@ rn.entity.instantiate = function(uuid, prefab_name)
 	local obj <close> = tz.profzone_obj:new()
 	obj:set_text(tostring(uuid))
 	obj:set_name("Instantiate - \"" .. prefab_name .. "\"")
+
+	local prefab = rn.entity.prefabs[prefab_name]
+	if prefab ~= nil then
+		if prefab.instantiate ~= nil then
+			prefab.instantiate(uuid)
+		end
+	else
+		tz.report("Missing prefab \"" .. prefab_name .. "\"")
+		tz.assert(false);
+	end
 
 	tz.report("instantiate " .. tostring(uuid) .. " from \"" .. prefab_name .. "\"")
 
