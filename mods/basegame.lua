cd67fe8adafef33ce520114ee97805d8ba05fe34
harrand <harrysjh98@gmail.com>
2024-02-06 02:54:44 +0000

    [lua.basegame] added keyboard_controlled prefab, which has basic keyboard controls. morbius now uses that other prefab. there is no prefab inheritance but you can just basically call into other prefabs like this. doesn't necessarily mean all prefabs support it

--- mods/basegame.lua
+++ mods/basegame.lua
@@ -5,30 +5,60 @@ rn.mods[mod] =
 	description = "Base game content for Red Nightmare",
 	prefabs =
 	{
+		keyboard_controlled =
+		{
+			instantiate = function(uuid)
+				local sc = rn.current_scene()
+				sc:entity_write(uuid, "control.forward", "w")
+				sc:entity_write(uuid, "control.left", "a")
+				sc:entity_write(uuid, "control.right", "d")
+				sc:entity_write(uuid, "control.backward", "s")
+			end,
+			update = function(uuid, delta_seconds)
+				local sc = rn.current_scene()
+				local x, y, z = sc:entity_get_local_position(uuid)
+				local inp = rn.input()
+
+				local control_forward = sc:entity_read(uuid, "control.forward")
+				local control_left = sc:entity_read(uuid, "control.left")
+				local control_right = sc:entity_read(uuid, "control.right")
+				local control_backward = sc:entity_read(uuid, "control.backward")
+
+				local movement_speed = 5.0
+				local moved = false
+
+				if inp:is_key_down(control_left) then
+					x = x - movement_speed * delta_seconds
+					moved = true
+				end
+				if inp:is_key_down(control_right) then
+					x = x + movement_speed * delta_seconds
+					moved = true
+				end
+				if inp:is_key_down(control_forward) then
+					y = y + movement_speed * delta_seconds
+					moved = true
+				end
+				if inp:is_key_down(control_backward) then
+					y = y - movement_speed * delta_seconds
+					moved = true
+				end
+				
+				if moved then
+					sc:entity_set_local_position(uuid, x, y, z);
+				end
+			end
+		},
 		morbius =
 		{
 			pre_instantiate = function(uuid)
 				return "plane"
 			end,
 			instantiate = function(uuid)
-
+				rn.entity.prefabs.keyboard_controlled.instantiate(uuid)
 			end,
 			update = function(uuid, delta_seconds)
-				local t = rn.current_scene():entity_read(uuid, "timer") or 0.0
-				t = t + delta_seconds
-
-				local x, y, z = rn.current_scene():entity_get_local_position(uuid)
-				if rn.input():is_key_down("a") then
-					x = x - 5.0 * delta_seconds
-				end
-				if rn.input():is_key_down("d") then
-					x = x + 5.0 * delta_seconds
-				end
-
-				if rn.input():is_mouse_down("right") then
-					x = 0; y = 0; z = 0
-				end
-				rn.current_scene():entity_set_local_position(uuid, x, y, z);
+				rn.entity.prefabs.keyboard_controlled.update(uuid, delta_seconds)
 			end
 		}
 	},
