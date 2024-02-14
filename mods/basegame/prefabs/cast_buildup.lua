17ebb669dd418effc784809f5c9565fc208617d4
harrand <harrysjh98@gmail.com>
2024-02-14 13:15:09 +0000

    [mod.basegame] added cast buildup entity. its just a spell-casting effect. you need to specify a `magic_type` on it, otherwise it will be default-white

---
+++ mods/basegame/prefabs/cast_buildup.lua
@@ -0,0 +1,32 @@
+rn.mods.basegame.prefabs.cast_buildup = 
+{
+	frame_count = 6,
+	static_init = function()
+		for i=0,rn.entity.prefabs.cast_buildup.frame_count,1 do
+			rn.renderer():add_texture("sprite.cast_buildup" .. i, "basegame/res/sprites/cast_buildup/cast_buildup" .. i .. ".png")
+		end
+	end,
+	pre_instantiate = function(uuid)
+		return rn.entity.prefabs.sprite.pre_instantiate(uuid)
+	end,
+	instantiate = function(uuid)
+		rn.entity.prefabs.sprite.set_texture(uuid, "sprite.cast_buildup0")
+	end,
+	update = function(uuid, delta_seconds)
+		local sc = rn.current_scene()
+		local t = sc:entity_read(uuid, "timer") or 0.0
+		t = t + delta_seconds
+		local frame_id = math.floor((t * 10.0) % rn.entity.prefabs.cast_buildup.frame_count)
+		rn.entity.prefabs.sprite.set_texture(uuid, "sprite.cast_buildup" .. frame_id)
+		sc:entity_write(uuid, "timer", t)
+
+		local magic_type = sc:entity_read(uuid, "magic_type")
+		if magic_type ~= nil then
+			local colour = rn.spell.schools[magic_type].colour
+			rn.entity.prefabs.sprite.set_colour(uuid, colour[1], colour[2], colour[3])
+		end
+	end,
+	on_collision = function(me, other)
+		return false
+	end
+}
