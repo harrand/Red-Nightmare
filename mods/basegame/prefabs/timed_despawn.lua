3fdb5d100ada2d37acdff157e99e00707f5a921a
harrand <harrysjh98@gmail.com>
2024-02-15 23:42:42 +0000

    [basegame.timed_despawn] added timed_despawn prefab

---
+++ mods/basegame/prefabs/timed_despawn.lua
@@ -0,0 +1,14 @@
+rn.mods.basegame.prefabs.timed_despawn = 
+{
+	-- set the internal variable "duration" to a number of seconds.
+	-- when it hits 0 the entity will automatically despawn.
+	update = function(uuid, delta_seconds)
+		local t = rn.current_scene():entity_read(uuid, "duration") or 999999999
+		t = t - delta_seconds
+		if t <= 0.0 then
+			rn.current_scene():remove_entity(uuid)
+		else
+			rn.current_scene():entity_write(uuid, "duration", t)
+		end
+	end
+}
