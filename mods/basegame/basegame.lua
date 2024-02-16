8de05915720b32198c14a80800fd9f07cb53ea51
harrand <harrysjh98@gmail.com>
2024-02-16 06:58:14 +0000

    [basegame.keyboard_controlled] keyboard_controlled now has a bunch of extra keybinds for casting spells. you have 9 actions, bound to their respective key on the keyboard (e.g pressing `1` casts whatever is bound to action.1). by default no actions are bound to spells. morbius prefab however binds `2` to the spell `morb`

--- mods/basegame/basegame.lua
+++ mods/basegame/basegame.lua
@@ -14,6 +14,7 @@ rn.mods.basegame =
 			end,
 			instantiate = function(uuid)
 				rn.entity.prefabs.keyboard_controlled.instantiate(uuid)
+				rn.entity.prefabs.keyboard_controlled.bind_spell(uuid, 2, "morb")
 				rn.current_scene():entity_write(uuid, "morbing", false)
 				rn.current_scene():entity_write(uuid, "age", 25)
 				rn.current_scene():entity_write(uuid, "timer", 0.0)
--- mods/basegame/prefabs/keyboard_controlled.lua
+++ mods/basegame/prefabs/keyboard_controlled.lua
@@ -7,6 +7,9 @@ rn.mods.basegame.prefabs.keyboard_controlled =
 		sc:entity_write(uuid, "control.right", "d")
 		sc:entity_write(uuid, "control.backward", "s")
 		sc:entity_write(uuid, "control.enabled", true)
+		for i=1,9,1 do
+			sc:entity_write(uuid, "control." .. tostring(i), tostring(i))
+		end
 	end,
 	update = function(uuid, delta_seconds)
 		local sc = rn.current_scene()
@@ -18,6 +21,22 @@ rn.mods.basegame.prefabs.keyboard_controlled =
 		local control_right = sc:entity_read(uuid, "control.right")
 		local control_backward = sc:entity_read(uuid, "control.backward")
 		local control_enabled = sc:entity_read(uuid, "control.enabled")
+
+		if control_enabled then
+			for i=1,9,1 do
+				local istr = tostring(math.floor(i))
+				local control_action = sc:entity_read(uuid, "control." .. istr)
+				-- we expect control_action to be a string representation of an *integer*
+				-- however if its 1, then the string could easily be `1.0`
+				-- so we string -> number -> string. i know...
+				if inp:is_key_down(tostring(math.floor(tonumber(control_action)))) then
+					local cast_name = sc:entity_read(uuid, "action." .. istr)
+					if cast_name ~= nil then
+						rn.spell.cast(uuid, cast_name)
+					end
+				end
+			end
+		end
 
 		local movement_speed = 5.0
 		local moved = false
@@ -42,5 +61,8 @@ rn.mods.basegame.prefabs.keyboard_controlled =
 		if moved then
 			sc:entity_set_local_position(uuid, x, y, z);
 		end
+	end,
+	bind_spell = function(uuid, action_id, spell_name)
+		rn.current_scene():entity_write(uuid, "action." .. tostring(math.floor(action_id)), spell_name)
 	end
 }
