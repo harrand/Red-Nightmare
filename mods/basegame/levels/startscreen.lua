889ef6f64a491b0628c451fefcce0f0d1c044b19
harrand <harrysjh98@gmail.com>
2024-03-04 23:03:40 +0000

    [lua.level] add datastore wrappers for level-specific data. this is cleared upon level load.

--- mods/basegame/levels/startscreen.lua
+++ mods/basegame/levels/startscreen.lua
@@ -6,7 +6,7 @@ rn.mods.basegame.levels.startscreen =
 		local player = rn.current_scene():add_entity("player_melistra")
 		rn.entity.prefabs.keyboard_controlled.set_enabled(player, false)
 
-		rn.data_store():set("player", player)
+		rn.level.data_write("player", player)
 
 		local w, h = tz.window():get_dimensions()
 		local ren = rn.renderer()
@@ -14,17 +14,17 @@ rn.mods.basegame.levels.startscreen =
 		local title_string_author = ren:add_string(w / 2 + 150, h * 0.8 - 55, 12, "by Harrand", 0.9, 0.3, 0.35)
 		local title_string_engine = ren:add_string(20, 60, 10, "Topaz" .. tz.version.string, 1.0, 1.0, 1.0)
 		local title_string_press_to_begin = ren:add_string(w / 2 - 110.0, h * 0.4, 10, "PRESS [ENTER] TO PLAY", 1.0, 1.0, 1.0)
-		rn.data_store():edit_some("title_string", title_string, "title_string_author", title_string_author, "title_string_press_to_begin", title_string_press_to_begin)
+		rn.level.data_write("title_string", title_string, "title_string_author", title_string_author, "title_string_press_to_begin", title_string_press_to_begin)
 	end,
 	update = function(delta_seconds)
 		local w, h = tz.window():get_dimensions()
 		local ren = rn.renderer()
-		local title_string, title_string_author, title_string_press_to_begin = rn.data_store():read_some("title_string", "title_string_author", "title_string_press_to_begin")
+		local title_string, title_string_author, title_string_press_to_begin = rn.level.data_read("title_string", "title_string_author", "title_string_press_to_begin")
 		ren:string_set_position(title_string, w / 2 - 250.0, h * 0.8)
 		ren:string_set_position(title_string_author, w / 2 + 150, h * 0.8 - 55)
 		ren:string_set_position(title_string_press_to_begin, w / 2 - 110.0, h * 0.4)
 
-		local player = rn.data_store():read("player")
+		local player = rn.level.data_read("player")
 		rn.entity.on_move(player, 0.0, -1.0, 0.0, delta_seconds)
 
 		if rn.input():is_key_down("enter") then
--- src/gamelib/lua/level.lua
+++ src/gamelib/lua/level.lua
@@ -3,6 +3,7 @@ rn.level = rn.level or {}
 rn.level.levels = rn.level.levels or {}
 
 rn.level.load = function(name)
+	rn.level.data_clear()
 	local level = rn.level.levels[name]
 	if level ~= nil then
 		rn.current_scene():clear_entities()
@@ -37,4 +38,42 @@ rn.level.current_level_update = function(delta_seconds)
 	if level.update ~= nil then
 		level.update(delta_seconds)
 	end
+end
+
+rn.level.data_read = function(...)
+	local args = table.pack(...)
+	local amended_args = {}
+	for i,key in pairs(args) do
+		if type(i) == 'number' then
+			key = "current_level." .. key
+			tz.report("key: " .. key)
+			table.insert(amended_args, key)
+		end
+	end
+	return rn.data_store():read_some(table.unpack(amended_args))
+end
+
+rn.level.data_write = function(...)
+	-- args: current_level, {key1, value1, key2, value2}
+	-- so if even, we're a key
+	local args = {...}
+	local amended_args = {}
+	local counter=0
+	for i,key_or_val in pairs(args) do
+		if type(i) == 'number' then
+			if counter % 2 == 0 then
+				-- even. we're a key and we want to amend our name
+				key_or_val = "current_level." .. key_or_val
+			end
+			counter = counter + 1
+			table.insert(amended_args, key_or_val)
+		end
+	end
+	rn.data_store():edit_some(table.unpack(amended_args))
+end
+
+rn.level.data_clear = function()
+	tracy.ZoneBegin()
+	rn.data_store():remove_all_of("current_level.")
+	tracy.ZoneEnd()
 end
