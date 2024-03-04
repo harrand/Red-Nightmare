4d1abbe85671288baffaa45dcd87a9b8467ae2bd
harrand <harrysjh98@gmail.com>
2024-03-04 13:02:26 +0000

    [engine] updated to fixed engine. all text now displays properly

--- mods/basegame/levels/startscreen.lua
+++ mods/basegame/levels/startscreen.lua
@@ -11,7 +11,7 @@ rn.mods.basegame.levels.startscreen =
 		rn.title_string = ren:add_string(w / 2 - 250.0, h * 0.8, 40, "Red Nightmare", 0.6, 0.1, 0.15)
 		rn.title_string_author = ren:add_string(w / 2 + 150, h * 0.8 - 55, 12, "by Harrand", 0.9, 0.3, 0.35)
 		rn.title_string_engine = ren:add_string(20, 60, 10, "Topaz" .. tz.version.string, 1.0, 1.0, 1.0)
-		rn.title_string_press_to_begin = ren:add_string(w / 2 - 110.0, h * 0.4, 10, "PRESS [ENTER] TO not PLAY", 1.0, 1.0, 1.0)
+		rn.title_string_press_to_begin = ren:add_string(w / 2 - 110.0, h * 0.4, 10, "PRESS [ENTER] TO PLAY", 1.0, 1.0, 1.0)
 	end,
 	update = function(delta_seconds)
 		local w, h = tz.window():get_dimensions()
