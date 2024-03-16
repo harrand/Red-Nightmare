1b3dcd0844af64b9c8d362a162d6ba4f9d5d4068
harrand <harrysjh98@gmail.com>
2024-03-16 01:01:25 +0000

    [basegame.iron_coif] added iron_coif. its just like iron_sallet, but chain!

--- mods/basegame/levels/startscreen.lua
+++ mods/basegame/levels/startscreen.lua
@@ -65,7 +65,7 @@ rn.mods.basegame.levels.startscreen =
 		rn.entity.prefabs.sprite.set_position(player, 0, (startscreen_lengthy * 0.5) - 20)
 		rn.entity.prefabs.keyboard_controlled.set_enabled(player, false)
 		rn.entity.prefabs.combat_stats.set_pct_more_movement_speed(player, -0.2)
-		rn.item.equip(player, "iron_sallet")
+		rn.item.equip(player, "iron_coif")
 		rn.item.equip(player, "iron_chainmail")
 		rn.item.equip(player, "iron_chainlegs")
 		rn.item.equip(player, "steel_longsword")
