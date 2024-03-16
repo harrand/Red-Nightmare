3978c4cd8ec0f778de2f9e1599db4bc465e5a335
harrand <harrysjh98@gmail.com>
2024-03-16 06:17:51 +0000

    [basegame] improved weapon models, and added an axe

---
+++ mods/basegame/prefabs/weapon_models/axe.lua
@@ -0,0 +1,15 @@
+rn.mods.basegame.prefabs.weapon_axe =
+{
+	static_init = function()
+		rn.renderer():add_model("weapon.axe", "basegame/res/models/axe.glb")
+	end,
+	pre_instantiate = function(uuid)
+		return "weapon.axe"
+	end,
+	instantiate = function(uuid)
+		local sc = rn.current_scene()
+		sc:entity_set_subobject_pixelated(uuid, 2, true)
+		sc:entity_set_subobject_visible(uuid, 2, true)
+	end,
+	update = rn.mods.basegame.prefabs.sticky.update
+}
