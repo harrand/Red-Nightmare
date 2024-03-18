a78b91eccf110b99aa0fb14f7f7b13aece9c4595
harrand <harrysjh98@gmail.com>
2024-03-18 02:00:39 +0000

    [zombie_devour] zombies will stop devouring if their victim dies during the cast. previously, if they had acquired a new target by the time the cast finishes, that new victim would instead be oneshot, even if its not close to melee range. zombies will also now devour anything allied with the player, instead of only the player

--- mods/basegame/spells/zombie_devour.lua
+++ mods/basegame/spells/zombie_devour.lua
@@ -5,12 +5,21 @@ rn.mods.basegame.spells.zombie_devour =
 	animation_override = "ZombieExecute",
 	description = "Bites into the flesh of the target, dealing massive physical damage. The target is stunned for the duration.",
 	finish = function(uuid, casterx, castery)
-		local target = rn.current_scene():entity_read(uuid, "target")
+		local target = rn.current_scene():entity_read(uuid, "devour_victim")
 		if target ~= nil and rn.current_scene():contains_entity(target) then
 			rn.entity.unstun(target)
 			local dmg = rn.entity.prefabs.combat_stats.get_base_physical_power(uuid) * 10
 			rn.entity.prefabs.combat_stats.dmg(target, dmg, "physical", uuid)
 			rn.entity.prefabs.combat_stats.heal(uuid, dmg * 0.5, "shadow", uuid)
+			rn.current_scene():entity_write(uuid, "devour_victim", nil)
+		end
+	end,
+	advance = function(uuid, cast_progress)
+		local victim = rn.current_scene():entity_read(uuid, "devour_victim")
+		-- if the victim despawned or died since we started devouring, then cancel this cast.
+		if victim == nil or not rn.current_scene():contains_entity(victim) or rn.entity.prefabs.combat_stats.is_dead(victim) then
+			rn.spell.clear(uuid)
+			rn.current_scene():entity_write(uuid, "devour_victim", nil)
 		end
 	end,
 	on_cast_begin = function(me)
@@ -18,6 +27,7 @@ rn.mods.basegame.spells.zombie_devour =
 		tz.assert(target ~= nil and rn.current_scene():contains_entity(target), "Attmept to precast zombie_devour on an entity that doesn't exist.")
 		-- set `target` to be stunned
 		rn.entity.stun(target, 10.0)
+		rn.current_scene():entity_write(me, "devour_victim", target)
 		-- make the victim face us.
 		local mex, mey = rn.entity.prefabs.sprite.get_position(me)
 		local vx, vy = rn.entity.prefabs.sprite.get_position(target)
